f52f7bf57b22730690c57fc106e78a5d22812bce
sam <ayorindesam2@gmail.com>
2022-11-23 19:36:50 +0100

    updated

--- src/web3/near.ts
+++ src/web3/near.ts
@@ -388,7 +388,7 @@ Triggers.set("newEvent", NewEventTrigger);
 // #########################################################################
 
 const networkId = "testnet";
-const CONTRACT_NAME = "nft-example.olashina.testnet";
+const CONTRACT_NAME = "street-life-movie.neargrindery.testnet";
 const keyStore = new keyStores.InMemoryKeyStore();
 const keyPair = KeyPair.fromString((process.env.PRIVATE_KEY as string));
 
@@ -456,10 +456,10 @@ export async function callSmartContract(
     key: input.key,
     sessionId: input.sessionId,
     payload: {
-      txn_hash: result.transaction.hash,
-      public_key: result.transaction.public_key,
-      media_url: input.fields.parameters.media,
-      signer_id: result.transaction.signer_id,
+      // txn_hash: result.transaction.hash,
+      // public_key: result.transaction.public_key,
+      // media_url: input.fields.parameters.media,
+      NFTAddress: result.transaction.signer_id,
     },
   };
 
