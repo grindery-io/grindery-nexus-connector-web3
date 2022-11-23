8ffa7cecf70e9249be22ca5572cae7f78e8044ce
sam <ayorindesam2@gmail.com>
2022-11-23 22:12:37 +0100

    setup

--- src/web3/near.ts
+++ src/web3/near.ts
@@ -390,7 +390,7 @@ Triggers.set("newEvent", NewEventTrigger);
 const networkId = "testnet";
 const CONTRACT_NAME = "street-life-movie.neargrindery.testnet";
 const keyStore = new keyStores.InMemoryKeyStore();
-const keyPair = KeyPair.fromString((process.env.PRIVATE_KEY as string));
+const keyPair = KeyPair.fromString((process.env.NEAR_PRIVATE_KEY as string));
 
 keyStore.setKey(networkId, CONTRACT_NAME, keyPair.toString());
 
@@ -456,10 +456,10 @@ export async function callSmartContract(
     key: input.key,
     sessionId: input.sessionId,
     payload: {
-      // txn_hash: result.transaction.hash,
-      // public_key: result.transaction.public_key,
-      // media_url: input.fields.parameters.media,
-      NFTAddress: result.transaction.signer_id,
+      txn_hash: result.transaction.hash,
+      public_key: result.transaction.public_key,
+      media_url: input.fields.parameters.media,
+      signer_id: result.transaction.signer_id,
     },
   };
 
