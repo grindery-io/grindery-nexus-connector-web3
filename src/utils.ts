ad12a4b7b07da3b966ce717a26d69206e3a76fe0
SAPikachu <i@sapika.ch>
2023-02-18 08:03:32 +0000

    Sanitize parameters

--- src/utils.ts
+++ src/utils.ts
@@ -2,24 +2,32 @@ import { ConnectorInput } from "grindery-nexus-common-utils/dist/connector";
 import { convert } from "./web3/evm/unitConverter";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export async function sanitizeParameters(input: ConnectorInput<any>) {
+async function sanitizeObject(parameters: Record<string, unknown>, input: ConnectorInput<any>) {
+  for (const key of Object.keys(parameters)) {
+    if (parameters[key] === "!!GRINDERY!!UNDEFINED!!") {
+      parameters[key] = undefined;
+    }
+    const unitConversionMode = parameters["_grinderyUnitConversion_" + key];
+    if (unitConversionMode) {
+      parameters[key] = await convert(parameters[key], unitConversionMode as string, input.fields, parameters);
+    }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function sanitizeParameters(input: ConnectorInput<any>, paramKeys = ["parameterFilters", "parameters"]) {
   if ("_grinderyChain" in input.fields) {
     input.fields.chain = input.fields._grinderyChain;
     delete input.fields._grinderyChain;
   }
-  for (const paramKey of ["parameterFilters", "parameters"]) {
+  for (const paramKey of paramKeys || []) {
     if (paramKey in input.fields) {
       const parameters = input.fields[paramKey];
-      for (const key of Object.keys(parameters)) {
-        if (parameters[key] === "!!GRINDERY!!UNDEFINED!!") {
-          parameters[key] = undefined;
-        }
-        const unitConversionMode = parameters["_grinderyUnitConversion_" + key];
-        if (unitConversionMode) {
-          parameters[key] = await convert(parameters[key], unitConversionMode, input.fields, parameters);
-        }
-      }
+      await sanitizeObject(parameters, input);
     }
+  }
+  if (!paramKeys?.length) {
+    await sanitizeObject(input.fields, input);
   }
   return input;
 }
