435c889ff59338d138aa3cf430abab13d9e5ddf4
SAPikachu <i@sapika.ch>
2023-02-18 07:30:38 +0000

    gnosisSafeSimpleTransfer

---
+++ src/web3/evm/connector/gnosisSafe/index.ts
@@ -0,0 +1,155 @@
+import { ActionOutput, ConnectorInput } from "grindery-nexus-common-utils";
+import { AbiItem } from "web3-utils";
+import { ethers } from "ethers";
+import mutexify from "mutexify/promise";
+import axios from "axios";
+import { NtaSigner } from "../../signer";
+import { SignTypedDataVersion } from "@metamask/eth-sig-util";
+import ABI from "./abi.json";
+import { getWeb3 } from "../../web3";
+
+export const execTransactionAbi: AbiItem = ABI.find((x) => x.name === "execTransaction") as AbiItem;
+
+const nonceMutexes: { [contractAddress: string]: () => Promise<() => void> } = {};
+
+export async function gnosisSafeSimpleTransfer(input: ConnectorInput<unknown>): Promise<ActionOutput> {
+  const parameters = input.fields as { [key: string]: string };
+  const dryRun = parameters.dryRun ?? false;
+  const authResp = await axios.post(
+    (process.env.CREDENTIAL_MANAGER_REQUEST_PREFIX || "").replace("$CDS_NAME", "safe") +
+      "grindery-nexus-orchestrator:3000/webhook/web3/callSmartContract/echo",
+    { safe: "{{ auth.safe }}", chainId: "{{ auth.chainId }}" },
+    {
+      headers: {
+        Authorization: `Bearer ${input.authentication}`,
+        "Content-Type": "application/json",
+        "x-grindery-template-scope": "all",
+      },
+    }
+  );
+  const contractAddress: string = authResp.data.safe;
+  parameters.to = String(parameters.to).trim();
+  let nonce = "0" as string | number;
+  const chainId = authResp.data.chainId;
+  if (!nonceMutexes[contractAddress]) {
+    nonceMutexes[contractAddress] = mutexify();
+  }
+  const { web3, close } = getWeb3(`eip155:${chainId}`);
+  const releaseLock = await nonceMutexes[contractAddress]();
+  try {
+    try {
+      const nonceResp = await axios.post(
+        `https://safe-client.gnosis.io/v2/chains/${chainId}/safes/${contractAddress}/multisig-transactions/estimations`,
+        { value: "0", operation: 0, to: parameters.to, data: "0x" }
+      );
+      nonce = nonceResp.data.recommendedNonce;
+    } catch (e) {
+      console.error("Failed to get nonce from Gnosis Safe: ", e, e.response?.data, parameters);
+      if (!dryRun) {
+        throw e;
+      }
+    }
+    console.debug(`[${contractAddress}] Nonce: ${nonce}`);
+    if (typeof nonce === "number") {
+      nonce = nonce.toString();
+    }
+    const params = [
+      parameters.to,
+      String(parameters.value),
+      "0x",
+      0,
+      "0",
+      "0",
+      "0",
+      "0x0000000000000000000000000000000000000000",
+      "0x0000000000000000000000000000000000000000",
+      nonce,
+    ];
+    let txHash: string;
+    try {
+      const contract = new web3.eth.Contract(ABI as unknown as AbiItem[], contractAddress);
+      txHash = await contract.methods.getTransactionHash(...params).call();
+    } catch (e) {
+      if (!dryRun) {
+        throw e;
+      }
+      txHash = "0x0000000000000000000000000000000000000000";
+    }
+    const [to, value, data, operation, safeTxGas, baseGas, gasPrice, gasToken, refundReceiver] = params;
+    const message = {
+      to,
+      value,
+      data,
+      operation,
+      safeTxGas,
+      baseGas,
+      gasPrice,
+      gasToken,
+      refundReceiver,
+      nonce,
+    };
+    message.to = ethers.utils.getAddress(String(message.to));
+    const signature = await NtaSigner.signTypedData({
+      data: {
+        types: {
+          EIP712Domain: [
+            {
+              type: "uint256",
+              name: "chainId",
+            },
+            {
+              type: "address",
+              name: "verifyingContract",
+            },
+          ],
+          SafeTx: [
+            { type: "address", name: "to" },
+            { type: "uint256", name: "value" },
+            { type: "bytes", name: "data" },
+            { type: "uint8", name: "operation" },
+            { type: "uint256", name: "safeTxGas" },
+            { type: "uint256", name: "baseGas" },
+            { type: "uint256", name: "gasPrice" },
+            { type: "address", name: "gasToken" },
+            { type: "address", name: "refundReceiver" },
+            { type: "uint256", name: "nonce" },
+          ],
+        },
+        primaryType: "SafeTx",
+        domain: {
+          chainId,
+          verifyingContract: contractAddress,
+        },
+        message,
+      },
+      version: SignTypedDataVersion.V4,
+    });
+    if (dryRun) {
+      return {
+        payload: {
+          safeTxHash: txHash,
+          signature,
+        },
+      };
+    }
+    try {
+      const resp = await axios.post(
+        `https://safe-client.gnosis.io/v1/chains/${chainId}/transactions/${contractAddress}/propose`,
+        {
+          origin: "Grindery Flow",
+          safeTxHash: txHash,
+          signature,
+          sender: await NtaSigner.getAddress(),
+          ...message,
+        }
+      );
+      return { payload: resp.data };
+    } catch (e) {
+      console.error("Failed to send transaction to Gnosis Safe: ", e, e.response?.data, message);
+      throw e;
+    }
+  } finally {
+    releaseLock();
+    close();
+  }
+}
--- src/web3/evm/signer.ts
+++ src/web3/evm/signer.ts
@@ -8,21 +8,21 @@ export const TypedDataVersion = SignTypedDataVersion;
 export class VaultSigner extends ethers.Signer {
   ethereumAddress = "";
 
-  constructor(provider?: ethers.providers.Provider) {
+  constructor(provider?: ethers.providers.Provider, private prefix = "eth") {
     super();
     ethers.utils.defineReadOnly(this, "provider", provider || undefined);
   }
 
   async getAddress(): Promise<string> {
     if (!this.ethereumAddress) {
-      const key = await callVault<string>("ethGetAddress");
+      const key = await callVault<string>(`${this.prefix}GetAddress`);
       this.ethereumAddress = key;
     }
     return Promise.resolve(this.ethereumAddress);
   }
 
   async signMessage(message: string | ethers.utils.Bytes): Promise<string> {
-    return await callVault("ethSignMessage", { message: ethers.utils.hexlify(message) });
+    return await callVault(`${this.prefix}SignMessage`, { message: ethers.utils.hexlify(message) });
   }
 
   async signTypedData<V extends SignTypedDataVersion, T extends MessageTypes>({
@@ -35,18 +35,20 @@ export class VaultSigner extends ethers.Signer {
     if (data === null || data === undefined) {
       throw new Error("Missing data parameter");
     }
-    return await callVault("ethSignTypedData", { data, version });
+    return await callVault(`${this.prefix}SignTypedData`, { data, version });
   }
 
   async signTransaction(transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>): Promise<string> {
     const unsignedTx = await ethers.utils.resolveProperties(transaction);
-    return await callVault("ethSignTransaction", { transaction: unsignedTx });
+    return await callVault(`${this.prefix}SignTransaction`, { transaction: unsignedTx });
   }
 
   connect(provider: ethers.providers.Provider): VaultSigner {
     return new VaultSigner(provider);
   }
 }
+
+export const NtaSigner = new VaultSigner(undefined, "ethNta");
 
 const vaultSigner = new VaultSigner();
 export default vaultSigner;
