d63fa083dd944e3130c84a7148f58b1f6a8b2c81
Joe Hu <sapikachu@gmail.com>
2023-04-20 10:20:49 +0000

    Another try

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # Fix error with BSC
 # https://github.com/ChainSafe/web3.js/issues/3936
 sed -i 's|tx.gas = utils.hexToNumber|tx.gas = (typeof tx.gas === "string" \&\& /^0x/i.test(tx.gas) \&\& tx.gas.length > Number.MAX_SAFE_INTEGER.toString(16).length + 1) ? outputBigNumberFormatter(tx.gas) : utils.hexToNumber|' node_modules/web3-core-helpers/lib/formatters.js
-sed -i 's|callback(errors.InvalidResponse(response))|callback(errors.InvalidResponse(error))|' node_modules/web3-providers-http/lib/index.js
+sed -i 's|callback(errors.InvalidResponse(response))|callback(error)|' node_modules/web3-providers-http/lib/index.js
