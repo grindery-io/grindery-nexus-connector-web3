c162caf0197055c72122937141d0384979d42fe2
SAPikachu <sapikachu@gmail.com>
2023-05-12 19:46:03 +0800

    New chains

--- src/web3/evm/chains.ts
+++ src/web3/evm/chains.ts
@@ -4,15 +4,17 @@ const ANKR = (name: string): [string, string] => [
   `wss://rpc.ankr.com/${name}/ws/${process.env.ANKR_KEY}`,
   `https://rpc.ankr.com/${name}/${process.env.ANKR_KEY}`,
 ];
-
 const ALCHEMY = (name: string): [string, string] => [
   `wss://${name}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
   `https://${name}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
 ];
-
 const GETBLOCK = (name: string, netType: "mainnet" | "testnet" | string = "mainnet"): [string, string] => [
   `wss://${name}.getblock.io/${process.env.GETBLOCK_API_KEY}/${netType}/`,
   `https://${name}.getblock.io/${process.env.GETBLOCK_API_KEY}/${netType}/`,
+];
+const LAVANET = (wsPath: string, httpsPath: string): [string, string] => [
+  `wss://g.w.lavanet.xyz:443/gateway/${wsPath}/${process.env.LAVANET_API_KEY}`,
+  `https://g.w.lavanet.xyz:443/gateway/${httpsPath}/${process.env.LAVANET_API_KEY}`,
 ];
 
 export const CHAIN_MAPPING: { [key: string]: [string, string] } = {
@@ -36,4 +38,6 @@ export const CHAIN_MAPPING: { [key: string]: [string, string] } = {
   "eip155:97": ANKR("bsc_testnet_chapel"),
   "eip155:4002": ANKR("fantom_testnet"),
   "eip155:1442": ANKR("polygon_zkevm_testnet"),
+  "eip155:44787": LAVANET("alfajores/rpc", "alfajores/rpc-http"),
+  "eip155:9000": LAVANET("evmost/json-rpc", "evmost/json-rpc-http"),
 };
