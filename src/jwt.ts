923805e66de7c1c7dae1b225d958f143ec44b938
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-06-20 11:39:52 +0200

    refactor export in jwt (#64)

--- src/jwt.ts
+++ src/jwt.ts
@@ -2,17 +2,13 @@ import { URL } from "node:url";
 import * as jose from "jose";
 import { getJwtTools, TypedJWTPayload } from "grindery-nexus-common-utils";
 
-const ISSUER = "urn:grindery:web3-driver";
-
-const jwtTools = getJwtTools(ISSUER);
+const jwtTools = getJwtTools("urn:grindery:web3-driver");
 jwtTools.getPublicJwk().catch((e) => {
   console.error("Failed to initialize keys:", e);
   process.exit(1);
 });
 
-const { encryptJWT, decryptJWT, signJWT, verifyJWT, getPublicJwk, typedCipher, typedToken, hmac } = jwtTools;
-export { encryptJWT, decryptJWT, signJWT, verifyJWT, getPublicJwk, typedCipher, typedToken, hmac };
-
+export const { encryptJWT, decryptJWT, signJWT, verifyJWT, getPublicJwk, typedCipher, typedToken, hmac } = jwtTools;
 export const FlowAddressToken = typedCipher("urn:grindery:web3-driver:flow-address-token");
 
 type AccessTokenExtra =
@@ -28,6 +24,7 @@ export type TAccessToken = TypedJWTPayload<AccessTokenExtra>;
 const ORCHESTRATOR_KEY = jose.createRemoteJWKSet(
   new URL(process.env.ORCHESTRATOR_PUBLIC_KEY || "https://orchestrator.grindery.org/oauth/jwks")
 );
+
 export async function parseUserAccessToken(token: string): Promise<TAccessToken> {
   const { payload } = await jose.jwtVerify(token, ORCHESTRATOR_KEY, {
     issuer: "urn:grindery:orchestrator",
