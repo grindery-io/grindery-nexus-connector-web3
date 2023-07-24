40d6d8ed9ef556ba85b3ecc2d7be800b91692e54
Thomas Coratger <60488569+tcoratger@users.noreply.github.com>
2023-07-24 10:51:46 +0200

    Update src/test/evm-utils.test.ts
    Co-authored-by: Joe Hu <sapikachu@gmail.com>

--- src/test/evm-utils.test.ts
+++ src/test/evm-utils.test.ts
@@ -25,7 +25,7 @@ describe("EVM utils tests", async function () {
     });
 
     it("Should return false if address B is null", async function () {
-      chai.expect(isSameAddress("0x71FA225B8F9Aeb50B44F96743275837F8EB7694E", null)).to.be.false;
+      chai.expect(isSameAddress("0x71fa225b8f9aeb50b44f96743275837f8eb7694e", null)).to.be.false;
     });
 
     it("Should return false if address A and address B are null", async function () {
