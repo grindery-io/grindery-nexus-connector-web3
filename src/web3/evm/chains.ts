ddd3e33a414288a91719341fc2302534fd784008
Joe Hu <sapikachu@gmail.com>
2025-03-29 08:46:34 +0000

    Disable more chains

--- src/web3/evm/chains.ts
+++ src/web3/evm/chains.ts
@@ -23,17 +23,17 @@ const CHAINSTACK = (nodeId: string, key?: string): [null, string] => [
 
 export const CHAIN_MAPPING: { [key: string]: [string | null, string] } = {
   "eip155:1": ANKR("eth"),
-  "eip155:42161": ANKR("arbitrum"),
+  // "eip155:42161": ANKR("arbitrum"),
   "eip155:100": ANKR("gnosis"),
   "eip155:137": ANKR("polygon"),
   // "eip155:42220": ANKR("celo"),
-  "eip155:43114": ANKR("avalanche"),
+  // "eip155:43114": ANKR("avalanche"),
   "eip155:56": ANKR("bsc"),
-  "eip155:250": ANKR("fantom"),
+  // "eip155:250": ANKR("fantom"),
   "eip155:1666600000": ANKR("harmony"),
   "eip155:25": GETBLOCK(process.env.GETBLOCK_ACCESS_TOKEN_WS, process.env.GETBLOCK_ACCESS_TOKEN_HTTPS),
   "eip155:1101": ANKR("polygon_zkevm"),
-  "eip155:1284": ANKR("moonbeam"),
+  // "eip155:1284": ANKR("moonbeam"),
 
   // Testnets
   "eip155:80002": ANKR("polygon_amoy"),
@@ -42,8 +42,8 @@ export const CHAIN_MAPPING: { [key: string]: [string | null, string] } = {
   // "eip155:4002": ANKR("fantom_testnet"),
   "eip155:1442": ANKR("polygon_zkevm_testnet"),
   "eip155:338": CHAINSTACK("nd-326-373-985"), // Cronos testnet
-  "eip155:44787": LAVANET("alfajores/rpc", "alfajores/rpc-http"),
-  "eip155:9000": LAVANET("evmost/json-rpc", "evmost/json-rpc-http"),
+  // "eip155:44787": LAVANET("alfajores/rpc", "alfajores/rpc-http"),
+  // "eip155:9000": LAVANET("evmost/json-rpc", "evmost/json-rpc-http"),
 };
 
 export const CHAIN_MAPPING_ACCOUNTING: { [key: string]: string } = {
